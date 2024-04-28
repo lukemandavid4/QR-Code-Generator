91baa5d23ec2ffd34e21094888fb2bac5aa6d6c8
lukemandavid4 <lukemandavid4@gmail.com>
2024-04-28 14:30:59 +0300

    updated

--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -1,17 +1,23 @@
 import './qrcode.css'
 
 const QrCode = () => {
+  const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+  let name = document.getElementById('name').value
+  let src
+  const handleClick = () =>{
+    src = url + name;
+  }
   return (
     <div className="qrcode">
       <form>
         <div className="qrcode__name">
             <label htmlFor="name">Enter text or URL</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name"/>
         </div>
         <div className="qrcode__img">
-            <img src="" />
+            <img src={src} />
         </div>
-        <button type="button">Generate QR Code</button>
+        <button type="button" onClick={handleClick}>Generate QR Code</button>
       </form>
     </div>
   )
