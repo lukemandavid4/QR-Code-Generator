51f23f0c4aa1b3f0ddbec497641ca5ccf5e93553
lukemandavid4 <lukemandavid4@gmail.com>
2024-04-28 14:41:15 +0300

    updated

--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -5,19 +5,19 @@ const QrCode = () => {
   const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
   const [name, setName] = useState('')
   const [src, SetSrc] = useState('')
-  const handleChange = (event) => {
-    setName(event.target.value);
+  const handleChange = (e) => {
+    setName(e.target.value);
   };
 
   const handleClick = () =>{
-    src = url + name;
+    SetSrc(url + name)
   }
   return (
     <div className="qrcode">
       <form>
         <div className="qrcode__name">
             <label htmlFor="name">Enter text or URL</label>
-            <input type="text" name="name" id="name"/>
+            <input type="text" name="name" id="name"onChange={handleChange}/>
         </div>
         <div className="qrcode__img">
             <img src={src} />
