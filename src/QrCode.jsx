82936b1120dd1ec0d5de780bff6ff198417d05bc
lukemandavid4 <lukemandavid4@gmail.com>
2024-04-28 14:37:08 +0300

    updated

--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './qrcode.css'
 
 const QrCode = () => {
   const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
-  let name = document.getElementById('name').value
-  let src
+  const [name, setName] = useState('')
+  const [src, SetSrc] = useState('')
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   const handleClick = () =>{
     src = url + name;
   }
